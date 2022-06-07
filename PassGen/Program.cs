92e719e48af72d1f488476349d19340f9cda9626
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2022-06-07 16:16:05 -0300

    -- Update
    Added more functions

--- PassGen/Program.cs
+++ PassGen/Program.cs
@@ -1,39 +1,53 @@
 ﻿using System;
+using System.Threading;
 
 namespace ExtremePassGenerator
 {
+
     internal class Program
     {
         static bool upper, lower, number, special;
         static int PasswordLenght;
+        static bool running = true;
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the Password Lenght: MIN 1 | MAX 50");
             PasswordLenght = Convert.ToInt32(Console.ReadLine());
 
             upper = lower = number = special = true;
-            if (PasswordLenght <= 50)
-            {
-                Console.WriteLine(PassGen.Password(
-                                                   upper,
-                                                   lower,
-                                                   number,
-                                                   special,
-                                                   PasswordLenght));
-            }
-            else
-            {
-                Console.WriteLine("Enter a valid number!");
-                Console.WriteLine("Closing...");
-            }
 
+
+            while (running)
+            {
+                if (PasswordLenght <= 50)
+                {
+                    Console.WriteLine(PassGen.Password(
+                                                       upper,
+                                                       lower,
+                                                       number,
+                                                       special,
+                                                       PasswordLenght));
+
+
+                    Console.WriteLine("New password in 5sec");
+                    Console.WriteLine();
+                    Thread.Sleep(5000);
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid number!");
+                    Console.WriteLine("Closing...");
+                }
+
+            }
         }
 
         public static class PassGen
         {
             private static string Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             private static string Numbers = "1234567890";
-            private static string Specials = "!@#$%&*()?^-";
+            private static string Specials = "!@#$%&*()?^-çÇ";
 
             public static string Password(
                 bool useUpper,
